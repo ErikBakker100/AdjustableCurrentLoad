ec588ef748e78fa5dd321c694ad8eafb58c824d4
Erik Bakker <e.e.bakker@online.nl>
2021-09-13 21:54:40 +0200

    Keyboard

--- Firmware/lib/_keypad/src/_keypad.h
+++ Firmware/lib/_keypad/src/_keypad.h
@@ -2,6 +2,7 @@
 
 #include <Arduino.h>
 #include <Keypad.h>
+#include "_pcf8574.h"
 
 const byte ROWS = 4; //four rows
 const byte COLS = 4; //three columns
@@ -14,3 +15,12 @@ char keys[ROWS][COLS] = {
 byte rowPins[ROWS] = {4, 3, 2, 1}; //connect to the row pinouts of the kpd
 byte colPins[COLS] = {8, 7, 6, 5}; //connect to the column pinouts of the kpd
 
+class _Keypad : public Keypad() {
+public:
+    void pin_mode(byte pinNum, byte mode);
+    void pin_write(byte pinNum, boolean level);
+    int pin_read(byte pinNum);
+    _Keypad();
+private:
+    PCF8574 *pcf8574;
+};
--- Firmware/src/main.cpp
+++ Firmware/src/main.cpp
@@ -2,7 +2,6 @@
 #include "tacho.h"
 #include "display.h"
 #include "_keypad.h"
-#include "_pcf8574.h"
 
 /*
 * Nodemcu board : pin number is equal to GPIO
@@ -20,7 +19,7 @@
 
 #define FAN_OVERRIDE 12
 tacho FanSpeed;
-PCF8574 pcf8574(PCF8574_Address, PCF8574_Interrupt_Pin, keyPressedOnPCF8574);
+
 Keypad keypad = Keypad(makeKeymap(keys), rowPins, colPins, ROWS, COLS );
 
 void check_if_exist_I2C();
@@ -34,16 +33,6 @@ void setup() {
   outputinfo("Initilizing\r\n");
 	Serial.begin(115200);
   check_if_exist_I2C();
-  pcf8574.pinMode(P0, INPUT);
-  pcf8574.pinMode(P1, INPUT);
-  pcf8574.pinMode(P2, INPUT);
-  pcf8574.pinMode(P3, INPUT);
-  pcf8574.pinMode(P4, INPUT);
-  pcf8574.pinMode(P5, INPUT);
-  pcf8574.pinMode(P6, INPUT);
-  pcf8574.pinMode(P7, INPUT);
-  if(pcf8574.begin()) {outputinfo("pcf8574 OK\r\n");}
-  else {outputinfo("pcf8574 FAULTY");}
   outputinfo("Init finalised\r\n");
   }
 
@@ -55,25 +44,6 @@ void loop() {
   delay(500);                     // wait
 //  outputinfo(String("Fan speed : "+ FanSpeed.rpm())+"rpm\r");
 //  char key = keypad.getKey();
-  PCF8574::DigitalInput di = pcf8574.digitalReadAll();
-//  char lees[9]{di.p0, di.p1, di.p2, di.p3, di.p4, di.p5, di.p6, di.p7, 0};
-//  outputinfo(String(lees)+"\r");
- 		Serial.print("READ VALUE FROM PCF P1: ");
-		Serial.print(di.p0);
-		Serial.print("-");
-		Serial.print(di.p1);
-		Serial.print("-");
-		Serial.print(di.p2);
-		Serial.print("-");
-		Serial.print(di.p3);
-		Serial.print("-");
-		Serial.print(di.p4);
-		Serial.print("-");
-		Serial.print(di.p5);
-		Serial.print("-");
-		Serial.print(di.p6);
-		Serial.print("-");
-		Serial.println(di.p7);
   if (keyPressed){
     keyPressed = false;
     outputinfo("KEY\r\n");
